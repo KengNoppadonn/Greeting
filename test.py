ecbc2472a4317cca30eea388770e3bbafbeb8e47
Noppadon Kaitsiri <noppadon@Noppadons-MacBook-Air.local>
2024-02-20 17:34:50 +0700

    can send picture

--- test.py
+++ test.py
@@ -7,20 +7,26 @@ import numpy as np
 flask_url = "http://127.0.0.1:8080/video_feed"
 
 def show_video():
+    
     while True:
+        
         # Request video stream from Flask server
         response = requests.get(flask_url, stream=True)
+        print(response)
         if not response.ok:
+            
             print("Error receiving video stream")
             break
-
+        
         # Read the frame as an image
-        image_bytes = BytesIO(response.content)
+        
+        image_bytes = BytesIO(response.content) #ติด
+        print("sjfkljdsjklsajfd")
         frame = cv2.imdecode(np.frombuffer(image_bytes.read(), np.uint8), -1)
-
+       
         # Display the frame
         cv2.imshow("Video Stream", frame)
-
+        
         # Exit on 'q' key press
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -28,4 +34,5 @@ def show_video():
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
+    
     show_video()
